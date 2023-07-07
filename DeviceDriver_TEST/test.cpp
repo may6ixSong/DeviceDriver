cc0a8f46e1535eb164128d03e8d21a19c2f47651
may6ix.song <smh92996275@gmail.com>
2023-07-07 11:33:24 +0900

    Read기능 구현

--- DeviceDriver_TEST/test.cpp
+++ DeviceDriver_TEST/test.cpp
@@ -1,9 +1,36 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "../DeviceDriver/DeviceDriver.cpp"
 using namespace testing;
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class FlashMemoryMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long), (override));
+	MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
+TEST(DeviceDriveTest, ReturnPassReadTest) {
+	long address = 0x40001000;
+	FlashMemoryMock mock;
+	EXPECT_CALL(mock, read(address))
+		.Times(5)
+		.WillRepeatedly(Return(unsigned char(0x80)));
+
+	DeviceDriver driver(&mock);
+
+	cout << driver.read(address) << endl;
+}
+
+TEST(DeviceDriveTest, ReturnErrorReadTest) {
+	long address = 0x40001000;
+	FlashMemoryMock device;
+	EXPECT_CALL(device, read(address))
+		.Times(2)
+		.WillOnce(Return(unsigned char(0x10)))
+		.WillRepeatedly(Return(unsigned char(0x80)));
+
+	DeviceDriver driver(&device);
+	EXPECT_THROW(driver.read(address), ReadException);
 }
